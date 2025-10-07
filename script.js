be7ebbf1e4a2df008664c8e68861a609005e7a28
ochieng-stack <68597481+ochieng-stack@users.noreply.github.com>
2025-10-07 12:38:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    // toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu")
+});
+ //close menu when the close button is clicked
+ menuCloseButton.addEventListener("click", ()=> menuOpenButton.click())
+
+// coding kitchen 
+
+
